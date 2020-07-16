35dcba101a4716a1f8ca780967a633ea5f68ce5a
Ishamudgalll <47916019+Ishamudgalll@users.noreply.github.com>
2020-07-16 16:09:36 +0530

    Update drowsy.py

--- drowsy.py
+++ drowsy.py
@@ -28,4 +28,6 @@ def upload():
 	return Response(response=img_processed,status=200,mimetype="application/json") # return json 
 
 #start server
-app.run(host="0.0.0.0",port=5000)
+#app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(debug=True)
